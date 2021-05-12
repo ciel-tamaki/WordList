86dd119af5123a03b9c44e04838ea99acb2d81ac
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-13 01:06:39 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 安念　玉希 on 2021/05/13.
+//  Copyright © 2021 安念　玉希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
