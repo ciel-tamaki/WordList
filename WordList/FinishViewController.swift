cec8d09b2f9de3bd7efd3790ad6c73d19199b824
tamaki annen <tmn.agenkir.luck@icloud.com>
2021-05-13 10:54:56 +0900

    単語帳の完成

---
+++ WordList/FinishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FinishViewController.swift
+//  WordList
+//
+//  Created by 安念　玉希 on 2021/05/13.
+//  Copyright © 2021 安念　玉希. All rights reserved.
+//
+
+import UIKit
+
+class FinishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WordList/ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by 安念　玉希 on 2021/05/13.
+//  Copyright © 2021 安念　玉希. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
